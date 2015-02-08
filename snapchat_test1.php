aafcabe4abafaffa3aaead099d10a46d40839e32
Winnie Wu <weiwu@college.harvard.edu>
2015-02-07 22:20:13 -0500

    uploading to clone to hcs

---
+++ snapchat_test1.php
@@ -0,0 +1,51 @@
+<?php
+
+define ('__ROOT__', dirname(__FILE__));
+require_once(__ROOT__.'/includes/lib/php-snapchat/src/snapchat.php');
+
+$snapchat = new Snapchat('harvardyak', 'h@rv@rdy4k!');
+
+$snapchat->updatePrivacy(Snapchat::PRIVACY_FRIENDS);
+
+$snaps = $snapchat->getSnaps();
+
+$added_friends = $snapchat->getAddedFriends();
+
+foreach($added_friends as $added_friend) {
+    $snapchat->addFriend($added_friend->name);
+}
+
+foreach ($snaps as $snap) {
+    $typearray = array(
+        Snapchat::MEDIA_VIDEO => '.mov',
+        Snapchat::MEDIA_IMAGE => '.jpg'
+    ); 
+    
+    // download the data
+    $data = $snapchat->getMedia($snap->id);
+         
+    $filename = __ROOT__ .
+        '/includes/downloads/'.
+        $snap->id.
+        $typearray[$snap->media_type]; 
+    
+    file_put_contents($filename, $data); 
+
+    // mark snap as viewed
+    $snapchat->markSnapViewed($snap->id);
+
+    //  upload as snap
+    $id = $snapchat->upload(
+       $snap->media_type, 
+       file_get_contents($filename)
+    );
+
+     // add to story   
+     $snapchat->setStory($id, $snap->media_type);
+
+    // for terminal testing
+    //echo "done";
+}
+
+// destroy evidence
+$snapchat->clearFeed();
