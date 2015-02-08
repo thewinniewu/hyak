dff280ad957d18b1dc2be610d4affa836602941d
Winnie Wu <weiwu@college.harvard.edu>
2015-02-08 11:02:14 -0500

    generalized script

--- cron/snapchat_test1.php
+++ cron/snapchat_test1.php
@@ -3,54 +3,71 @@
 define ('__ROOT__', dirname(dirname(__FILE__)));
 require_once(__ROOT__.'/includes/lib/php-snapchat/src/snapchat.php');
 
-$snapchat = new Snapchat('harvardyak', 'h@rv@rdy4k!');
 
-$snapchat->updatePrivacy(Snapchat::PRIVACY_FRIENDS);
+// usernames and passwords
+// should be replaced with db eventually
 
-$snaps = $snapchat->getSnaps();
+$snapgroups = array(
+    array(
+        "name" => "harvardyak",
+        "pw" => "h@rv@rdy4k!"
+    ),
+    array(
+        "name" => "hackatbrown",
+        "pw" => "h@ckatbr0wn",
+    ),
+);
+ 
+foreach ($snapgroups as $snapgroup) {
+    $snapchat = new Snapchat($snapgroup["name"], $snapgroup["pw"]);
 
-$added_friends = $snapchat->getAddedFriends();
+    $snapchat->updatePrivacy(Snapchat::PRIVACY_FRIENDS);
 
-foreach($added_friends as $added_friend) {
-    $snapchat->addFriend($added_friend->name);
+    $snaps = $snapchat->getSnaps();
+
+    $added_friends = $snapchat->getAddedFriends();
+
+    foreach($added_friends as $added_friend) {
+        $snapchat->addFriend($added_friend->name);
+    }
+
+    foreach ($snaps as $snap) {
+       var_dump($snap); 
+
+        $typearray = array(
+            Snapchat::MEDIA_VIDEO => '.mov',
+            Snapchat::MEDIA_IMAGE => '.jpg'
+        ); 
+        
+        // download the data
+        $data = $snapchat->getMedia($snap->id);
+             
+        $filename = __ROOT__ .
+            '/includes/downloads/'.
+            $snap->id.
+            $typearray[$snap->media_type]; 
+        
+        file_put_contents($filename, $data); 
+
+        // mark snap as viewed
+        //$snapchat->markSnapViewed($snap->id);
+
+        //  upload as snap
+        $id = $snapchat->upload(
+           $snap->media_type, 
+           file_get_contents($filename)
+        );
+       
+        // delete the files
+         unlink($filename);
+
+         // add to story   
+         $snapchat->setStory($id, $snap->media_type);
+
+        // for terminal testing
+        echo "done";
+    }
+
+    // destroy evidence
+    $snapchat->clearFeed();
 }
-
-foreach ($snaps as $snap) {
-   var_dump($snap); 
-
-    $typearray = array(
-        Snapchat::MEDIA_VIDEO => '.mov',
-        Snapchat::MEDIA_IMAGE => '.jpg'
-    ); 
-    
-    // download the data
-    $data = $snapchat->getMedia($snap->id);
-         
-    $filename = __ROOT__ .
-        '/includes/downloads/'.
-        $snap->id.
-        $typearray[$snap->media_type]; 
-    
-    file_put_contents($filename, $data); 
-
-    // mark snap as viewed
-    //$snapchat->markSnapViewed($snap->id);
-
-    //  upload as snap
-    $id = $snapchat->upload(
-       $snap->media_type, 
-       file_get_contents($filename)
-    );
-   
-    // delete the files
-     unlink($filename);
-
-     // add to story   
-     $snapchat->setStory($id, $snap->media_type);
-
-    // for terminal testing
-    echo "done";
-}
-
-// destroy evidence
-$snapchat->clearFeed();
